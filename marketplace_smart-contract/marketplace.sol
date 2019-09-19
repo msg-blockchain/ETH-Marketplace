624000b12cfa3312a9054155eb756e012eea8e7a
julian-bauer-msg <Julian.Bauer@msg.group>
2019-09-19 15:55:40 +0200

    Complete Revision

--- marketplace_smart-contract/marketplace.sol
+++ marketplace_smart-contract/marketplace.sol
@@ -1,45 +1,92 @@
+//Set the Version of Solidity that is used
 pragma solidity 0.5.11;
 
 /**TODO
- * createNewUser(): only specific addresses should be able to create a user, also just one per address
  * new function: show list of all Users
  * add item list / "inventory" for Users
  * Error Messages
  */
 
-//Contract for Marketplace
+//Smart Contract for Marketplace
 contract Marketplace {
+    
+    //Constructor wil be automatcally excetcuted when Contract is called
+    constructor() public {
+        //Send all the Market Currency to the Contract Address
+        balanceOf[contractAddress] = totalSupply;
+        //Log this Transfer to the Bockchain
+        emit Transfer(address(0), contractAddress, totalSupply);
+    }
 
-//User----------------------------------------------
+//User related Variables and Functions
 
-    //Initial Coins a User gets when creating an Account
-    uint256 initialCoins = 500;
+    //Initial Coins a User gets when creating an Account on the Marketplace
+    uint initialCoins = 500;
 
-    //Counting Users
-    uint256 public userCount = 0;
+    //Variable counting Users
+    uint public userCount = 0;
 
     //User Instance
     struct User {
-        //Person ID
+        //User ID
         uint userId;
-        //User name
+        //User Name
         string name;
-        //User address
+        //User Address
         address userAddress;
-        //Sellable items
     }
 
-    //Mapping with all Users
+    //Mapping of all Users
     mapping(uint => User) public allUsers;
+    
+    //Show a Users balance by his ID
+    function balanceById(uint userId) public view returns (uint) {
+        //Find User Address by User ID
+        address _address = allUsers[userId].userAddress;
+        return getBalance(_address);
+    }
 
-//Auction--------------------------------------------
+    //Create a new User, every User has to be created from a different Address
+    function createNewUser(string memory _name) public{
+        //Address of User Creator
+        address senderAddress = msg.sender;
+        bool exists = false;
+        //Check if User already exists
+        for (uint i = 0; i < userCount; i++) {
+            if (allUsers[i].userAddress == senderAddress) {
+                exists = true;
+            }
+        }
+        require(exists == false, "User Creation failed because Address is already used.");
+        //Create Entry in allUsers
+        allUsers[userCount] = User(userCount, _name, senderAddress);
+        //Create Entry in MarketCurrency Smart Contract
+        transferFromContract(msg.sender, initialCoins);
+        userCount += 1;
+    }
+    
+    //Show a User by User ID
+    function showUser(uint index) public view returns (uint, string memory, address, uint) {
+        return (allUsers[index].userId, allUsers[index].name, allUsers[index].userAddress, balanceOf[allUsers[index].userAddress]);
+    }
 
-    //Counting Orders
-    uint256 public orderCount = 0;
+    //Get User ID by Address
+    function getUserIdByAddress() public view returns (uint) {
+        for (uint i = 0; i < userCount; i++) {
+            if (allUsers[i].userAddress == msg.sender) {
+                return (i);
+            }
+        }
+    }
+
+//Order related Variables and Functions
+
+    //Variable counting Orders
+    uint public orderCount = 0;
 
     //Order Instance
     struct Order {
-        //Order Creator, points to a User in allusers, similar to userCount
+        //User ID of Order Creator
         uint orderCreator;
         //Order ID
         uint orderId;
@@ -49,70 +96,73 @@ contract Marketplace {
         bool buy;
         //Order Price
         uint orderPrice;
+        //Order Status, becomes inactive when filled
+        bool active;
     }
 
-    //Mapping with all Orders
+    //Mapping of all Orders
     mapping(uint => Order) public allOrders;
-
-//Currency--------------------------------------------
-
-    //Show User balance
-    function balanceById(uint256 userId) public view returns (uint) {
-        address _address = allUsers[userId].userAddress;
-        getBalance(_address);
-    }
-
-//Marketplace Functions---------------------------------
-
-    //Create new User, every User has to be created from a different Address
-    function createNewUser(string memory _name) public{
-        address senderAddress = msg.sender;
-        bool exists = false;
-        //Check if User already exists
-        for (uint i = 0; i < userCount; i++) {
-            if (allUsers[i].userAddress == senderAddress) {
-                exists = true;
-            }
-        }
-        require(exists = false, "User Creation failed because Address is already used.");
-        //Create Entry in List of all Users
-        allUsers[userCount] = User(userCount, _name, senderAddress);
-        //Create Entry in MarketCurrency Smart Contract
-        transferFromContract(msg.sender, initialCoins);
-        userCount += 1;
-    }
-
-    //Show a User
-    function showUser(uint index) public view returns (uint, string memory, address, uint256) {
-        return (allUsers[index].userId, allUsers[index].name, allUsers[index].userAddress, balanceOf[allUsers[index].userAddress]);
-    }
-
-    //Get User by Address
-    function getUserByAddress() public view returns (uint) {
-        for (uint i = 0; i < userCount; i++) {
-            if (allUsers[i].userAddress == msg.sender) {
-                uint userNumber = i+1;
-                return (userNumber);
-            }
-        }
-    }
 
     //Create Order
     function createOrder(string memory _Name, bool _Buy, uint _Price) public {
-        uint256 orderCreator = getUserByAddress();
+        uint orderCreator = getUserIdByAddress();
         //Create Entry in List of all Orders
-        allOrders[orderCount] = Order(orderCreator, orderCount, _Name, _Buy, _Price);
+        allOrders[orderCount] = Order(orderCreator, orderCount, _Name, _Buy, _Price, true);
         orderCount += 1;
     }
     
-    //Market Currency-----------------------------------------------------------------
+    //Show a specific Order
+    function showOrder(uint _orderNumber) public view returns (string memory, string memory, uint, string memory){
+        string memory orderType;
+        if (allOrders[_orderNumber].buy == true) {
+            orderType = "Sell Order";
+        }
+        else {
+            orderType = "Buy Order";
+        }
+        string memory orderActive;
+        if (allOrders[_orderNumber].active == true) {
+            orderActive = "Order is active";
+        }
+        else {
+            orderActive = "Order is inactive";
+        }
+        return (orderType, allOrders[_orderNumber].orderName, allOrders[_orderNumber].orderPrice, orderActive);
+    }
+    
+    //Fill a specific Order
+    function fillOrder(uint _orderNumber) public {
+        //Check if Order is active
+        require(allOrders[_orderNumber].active == true, "Order has already been filled!");
+        //Check if OrderCreator and OrderFiller are the same Person
+        uint orderCreator = allOrders[_orderNumber].orderCreator;
+        uint orderFiller = getUserIdByAddress();
+        require(orderCreator != orderFiller, "You cannot fill your own Order!");
+        //Check if Order Creator or Order Filler has enough Funds
+        uint costs = allOrders[_orderNumber].orderPrice;
+        //Check if Buy or Sell Order
+        bool typeBuy = allOrders[_orderNumber].buy;
+        if (typeBuy == true) {
+            require(getBalance(allUsers[orderCreator].userAddress) >= costs, "Order Creator has insufficient Funds!");
+            //Send Funds from Order Creator to Order Filler
+            transferFrom(allUsers[orderCreator].userAddress, allUsers[orderFiller].userAddress, costs);
+            }
+        else {
+            require(getBalance(allUsers[orderFiller].userAddress) >= costs, "Order Filler has insufficient Funds!");
+            //Send Funds from Order Filler to Order Creator
+            transferFrom(allUsers[orderFiller].userAddress, allUsers[orderCreator].userAddress, costs);
+        }
+        allOrders[_orderNumber].active == false;
+    }
+    
+//Market Currency related Variables and Functions
     
     //Total Number of Tokens
-    uint256 public totalSupply = 1000000;
+    uint public totalSupply = 1000000;
     //Name of Token
-    string public name;
-    //Symbol / Ticker of the Token
-    string public symbol;
+    string public name = "Market Token";
+    //Symbol of the Token
+    string public symbol = "MT";
 
     //Token Transfer Event
     event Transfer(address indexed _from, address indexed _to, uint _value);
@@ -121,23 +171,13 @@ contract Marketplace {
     event Approve(address indexed _owner, address indexed _spender, uint _value);
 
     //Addresses and associated Balances
-    mapping(address => uint256) public balanceOf;
+    mapping(address => uint) public balanceOf;
 
     //Addresses and associated Allowances
-    mapping(address => mapping(address => uint256)) public allowance;
+    mapping(address => mapping(address => uint)) public allowance;
     
     //Contract address
     address contractAddress = address(this);
-
-//Functions
-
-    //Create the Market Currency
-    function createCurrency(string memory _Name, string memory _Symbol) public {
-        name = _Name;
-        symbol = _Symbol;
-        balanceOf[contractAddress] = totalSupply;
-        emit Transfer(address(0), contractAddress, totalSupply);
-    }
 
     //Transfer Tokens from Contract
     function transferFromContract(address _receiver, uint _value) public returns (bool success) {
@@ -158,7 +198,7 @@ contract Marketplace {
     }
 
     //Approve a _Spender to send a specific _Value from your Address
-    function approve(address _spender, uint256 _value) public returns (bool success) {
+    function approve(address _spender, uint _value) public returns (bool success) {
         allowance[msg.sender][_spender] = _value;
         emit Approve(msg.sender, _spender, _value);
         return true;
@@ -167,7 +207,7 @@ contract Marketplace {
     //Transfer Tokens from one Address to another Address
     function transferFrom(address _from, address _to, uint _value) public returns (bool success) {
         require(_value <= balanceOf[_from]);
-        require(_value <= allowance[_from][msg.sender]);
+        //require(_value <= allowance[_from][msg.sender]); Not yet implemented
         balanceOf[_from] -= _value;
         balanceOf[_to] += _value;
         allowance[_from][msg.sender] -= _value;
@@ -179,15 +219,15 @@ contract Marketplace {
     function getBalance(address _checkAddress) public view returns (uint) {
         return balanceOf[_checkAddress];
     }
-    
+
     //Assets-----------------------------------------------------------------------------------
 
     //Name of Asset
     string public AssetName;
 
     //Counting Assets
-    uint256 public AssetCounter = 0;
-    
+    uint public AssetCounter = 0;
+
     //Token Transfer Event
     event AssetTransfer(address indexed _from, address indexed _to, uint _value);
 
@@ -195,21 +235,22 @@ contract Marketplace {
     event AssetApprove(address indexed _owner, address indexed _spender, uint _value);
 
     //Asset IDs and Names
-    mapping(uint256 => string) AssetList;
+    mapping(uint => string) AssetList;
 
     //Addresses and associated Assets and Balances
-    mapping(address => mapping(uint256 => uint256)) public AssetBalanceOf;
+    mapping(address => mapping(uint => uint)) public AssetBalanceOf;
 
     //Addresses and associated Allowances
-    mapping(address => mapping(address => uint256)) public assetAllowance;
-    
-//Functions
+    mapping(address => mapping(address => uint)) public assetAllowance;
 
+//Functions
+    /*
     //IN PROGRESS==============================================
 
     //Create Asset
     function createAsset(string memory _Name, string memory _Symbol) public {
         name = _Name;
+        symbol = _Symbol;
         balanceOf[contractAddress] = totalSupply;
         emit Transfer(address(0), contractAddress, totalSupply);
     }
@@ -233,7 +274,7 @@ contract Marketplace {
     }
 
     //Approve a _Spender to send a specific Asset _Value from your Address
-    function approveAsset(address _spender, uint256 _value) public returns (bool success) {
+    function approveAsset(address _spender, uint _value) public returns (bool success) {
         allowance[msg.sender][_spender] = _value;
         emit Approve(msg.sender, _spender, _value);
         return true;
@@ -249,4 +290,5 @@ contract Marketplace {
         emit Transfer(_from, _to, _value);
         return true;
     }
+    */
 }
