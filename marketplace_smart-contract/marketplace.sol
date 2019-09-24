a079f80de72ea3c65fd3fcf98cd7ecd1ed13482f
julian-bauer-msg <Julian.Bauer@msg.group>
2019-09-24 11:12:31 +0200

    Safety improvement, Event emit implementation

--- marketplace_smart-contract/marketplace.sol
+++ marketplace_smart-contract/marketplace.sol
@@ -2,9 +2,7 @@
 pragma solidity 0.5.11;
 
 /**TODO
- * new function: show list of all Users
- * add item list / "inventory" for Users
- * Error Messages
+ * Allowance implementation
  */
 
 //Smart Contract for Marketplace
@@ -43,15 +41,15 @@ contract Marketplace {
     function balanceById(uint userId) public view returns (uint) {
         //Find User Address by User ID
         address _address = allUsers[userId].userAddress;
-        return getBalance(_address);
+        return balanceOf[_address];
     }
 
     //Create a new User, every User has to be created from a different Address
     function createNewUser(string memory _name) public{
         //Address of User Creator
         address senderAddress = msg.sender;
+        //Check if a User has already been created from this Address
         bool exists = false;
-        //Check if User already exists
         for (uint i = 0; i < userCount; i++) {
             if (allUsers[i].userAddress == senderAddress) {
                 exists = true;
@@ -71,7 +69,7 @@ contract Marketplace {
     }
 
     //Get User ID by Address
-    function getUserIdByAddress() public view returns (uint) {
+    function getUserIdByAddress() private view returns (uint) {
         for (uint i = 0; i < userCount; i++) {
             if (allUsers[i].userAddress == msg.sender) {
                 return (i);
@@ -106,6 +104,12 @@ contract Marketplace {
     //Create Order
     function createOrder(string memory _Name, bool _Buy, uint _Price) public {
         uint orderCreator = getUserIdByAddress();
+        //If Buy Order, check if User has enough Currency and lock the Amount
+        if (_Buy == true) {
+          require(balanceOf[msg.sender] >= _Price, "Not enough Currency to create Order!");
+          require(balanceOf[msg.sender] - allowance[msg.sender][contractAddress] >= _Price, "Already too much Currency in other Orders!");
+          approve(contractAddress, _Price);
+        }
         //Create Entry in List of all Orders
         allOrders[orderCount] = Order(orderCreator, orderCount, _Name, _Buy, _Price, true);
         orderCount += 1;
@@ -143,14 +147,19 @@ contract Marketplace {
         //Check if Buy or Sell Order
         bool typeBuy = allOrders[_orderNumber].buy;
         if (typeBuy == true) {
-            require(getBalance(allUsers[orderCreator].userAddress) >= costs, "Order Creator has insufficient Funds!");
+            require(balanceOf[allUsers[orderCreator].userAddress] >= costs, "Order Creator has insufficient Funds!");
+            require(balanceOf[allUsers[orderCreator].userAddress] - allowance[allUsers[orderCreator].userAddress][contractAddress] >= costs, "Order Creator already has too much Currency in other Orders!");
             //Send Funds from Order Creator to Order Filler
-            transferFrom(allUsers[orderCreator].userAddress, allUsers[orderFiller].userAddress, costs);
+            transferFrom(allUsers[orderCreator].userAddress, contractAddress, costs);
+            allowance[allUsers[orderCreator].userAddress][contractAddress] -= costs;
+            transferFromContract(msg.sender, costs);
             }
         else {
-            require(getBalance(allUsers[orderFiller].userAddress) >= costs, "Order Filler has insufficient Funds!");
+            require(balanceOf[allUsers[orderFiller].userAddress] >= costs, "Order Filler has insufficient Funds!");
+            require(balanceOf[msg.sender] - allowance[msg.sender][contractAddress] >= costs, "Order Filler already has too much Currency in other Orders!");
             //Send Funds from Order Filler to Order Creator
-            transferFrom(allUsers[orderFiller].userAddress, allUsers[orderCreator].userAddress, costs);
+            transferFrom(allUsers[orderFiller].userAddress, contractAddress, costs);
+            transferFromContract(allUsers[orderCreator].userAddress, costs);
         }
         allOrders[_orderNumber].active = false;
     }
@@ -180,7 +189,7 @@ contract Marketplace {
     address contractAddress = address(this);
 
     //Transfer Tokens from Contract
-    function transferFromContract(address _receiver, uint _value) public returns (bool success) {
+    function transferFromContract(address _receiver, uint _value) private returns (bool success) {
         require(balanceOf[contractAddress] >= _value);
         balanceOf[contractAddress] -= _value;
         balanceOf[_receiver] += _value;
@@ -205,90 +214,12 @@ contract Marketplace {
     }
 
     //Transfer Tokens from one Address to another Address
-    function transferFrom(address _from, address _to, uint _value) public returns (bool success) {
-        require(_value <= balanceOf[_from]);
-        //require(_value <= allowance[_from][msg.sender]); Not yet implemented
+    function transferFrom(address _from, address _to, uint _value) private returns (bool success) {
+        require(_value <= balanceOf[_from], "Not enough Funds!");
+        require(_value <= allowance[_from][contractAddress], "Too much Funds reserved in Orders!");
         balanceOf[_from] -= _value;
         balanceOf[_to] += _value;
-        allowance[_from][msg.sender] -= _value;
         emit Transfer(_from, _to, _value);
         return true;
     }
-
-    //Check Balance of an Address
-    function getBalance(address _checkAddress) public view returns (uint) {
-        return balanceOf[_checkAddress];
-    }
-
-    //Assets-----------------------------------------------------------------------------------
-
-    //Name of Asset
-    string public AssetName;
-
-    //Counting Assets
-    uint public AssetCounter = 0;
-
-    //Token Transfer Event
-    event AssetTransfer(address indexed _from, address indexed _to, uint _value);
-
-    //Approve Event -> Allow someone to send Tokens from your Address
-    event AssetApprove(address indexed _owner, address indexed _spender, uint _value);
-
-    //Asset IDs and Names
-    mapping(uint => string) AssetList;
-
-    //Addresses and associated Assets and Balances
-    mapping(address => mapping(uint => uint)) public AssetBalanceOf;
-
-    //Addresses and associated Allowances
-    mapping(address => mapping(address => uint)) public assetAllowance;
-
-//Functions
-    /*
-    //IN PROGRESS==============================================
-
-    //Create Asset
-    function createAsset(string memory _Name, string memory _Symbol) public {
-        name = _Name;
-        symbol = _Symbol;
-        balanceOf[contractAddress] = totalSupply;
-        emit Transfer(address(0), contractAddress, totalSupply);
-    }
-
-    //Transfer Asset from Contract
-    function transferAssetFromContract(address _receiver, uint _value) public returns (bool success) {
-        require(balanceOf[contractAddress] >= _value);
-        balanceOf[contractAddress] -= _value;
-        balanceOf[_receiver] += _value;
-        emit Transfer(contractAddress, _receiver, _value);
-        return true;
-    }
-    
-    //Transfer Asset to Someone
-    function transferAsset(address _receiver, uint _value) public returns (bool success) {
-        require(balanceOf[msg.sender] >= _value);
-        balanceOf[msg.sender] -= _value;
-        balanceOf[_receiver] += _value;
-        emit Transfer(msg.sender, _receiver, _value);
-        return true;
-    }
-
-    //Approve a _Spender to send a specific Asset _Value from your Address
-    function approveAsset(address _spender, uint _value) public returns (bool success) {
-        allowance[msg.sender][_spender] = _value;
-        emit Approve(msg.sender, _spender, _value);
-        return true;
-    }
-
-    //Transfer Asset from one Address to another Address
-    function transferAssetFrom(address _from, address _to, uint _value) public returns (bool success) {
-        require(_value <= balanceOf[_from]);
-        require(_value <= allowance[_from][msg.sender]);
-        balanceOf[_from] -= _value;
-        balanceOf[_to] += _value;
-        allowance[_from][msg.sender] -= _value;
-        emit Transfer(_from, _to, _value);
-        return true;
-    }
-    */
 }
