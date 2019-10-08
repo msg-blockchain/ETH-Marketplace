2a47fcb4b8649ad0721e538604156ffa990997ab
julian-bauer-msg <54405750+julian-bauer-msg@users.noreply.github.com>
2019-10-08 13:32:56 +0200

    Add files via upload

--- docs/main.js
+++ docs/main.js
@@ -551,26 +551,35 @@ const abi = [
 
 
 let contract;
-window.addEventListener('load', () => {
-    if(typeof(web3) == 'undefined') {
-        return console.log("Metamask is not installed");
+window.addEventListener('load', async () => {
+    // Modern dapp browsers...
+    if (window.ethereum) {
+      window.web3 = new Web3(ethereum);
+      try {
+        // Request account access if needed
+        await ethereum.enable();
+        // Acccounts now exposed
+        web3.eth.sendTransaction({/* ... */});
+      } catch (error) {
+        // User denied account access...
+      }
     }
+    // Legacy dapp browsers...
+    else if (window.web3) {
+      window.web3 = new Web3(web3.currentProvider);
+      // Acccounts always exposed
+      web3.eth.sendTransaction({/* ... */});
+    }
+    // Non-dapp browsers...
     else {
-        console.log("Metamask is installed");
+      console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
     }
     
     contract = web3.eth.contract(abi).at(contract_address);
 
 
-    //var user_account = web3.eth.accounts;
-    //console.log(user_account);
-
-    web3.eth.getBlock(48, function(error, result){
-        if(!error)
-            console.log(JSON.stringify(result));
-        else
-            console.error(error);
-     })
+    var user_account = web3.eth.accounts;
+    console.log(user_account);
 
     //contract.balanceById(0).call((error, result) => {
     //    if(!error) {
